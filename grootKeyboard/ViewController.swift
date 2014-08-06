b6dd15865b0ef44d91f9116094043b4d63b5428f
Kote <kmushegi@bowdoin.edu>
2014-08-06 10:40:21 +0400

    minor improvements

--- grootKeyboard/ViewController.swift
+++ grootKeyboard/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //textField.becomeFirstResponder();
+        
+        textField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
